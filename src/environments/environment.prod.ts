bfa06d68e8f40028b98852a57f8788e6676f2459
pacesetter <pacesetter4lyf@gmail.com>
2024-05-09 11:03:45 -0400

    changing api url from heroku to render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   // apiUrl: 'http://localhost:3001/api/v1',
   // apiUrl: "http://familia-env.eba-gtmaxuqf.us-east-2.elasticbeanstalk.com/api/v1"
   // apiUrl: 'https://familia-bfa6f53c2f10.herokuapp.com/api/v1',
-  apiUrl: 'https://familia-backend.onrender.com/api/vi',
+  apiUrl: 'https://familia-backend.onrender.com/api/v1',
 };
